948113bd61788369de0b7c86490d44e9aeaaad77
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-11-18 20:18:04 +0900

    feat: add assoiation and validation on Flashcard model #29

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_18_105839) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_18_111128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_18_105839) do
     t.string "description"
     t.string "title"
     t.string "icon_color"
-    t.string "language"
+    t.string "language", default: "EN"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
