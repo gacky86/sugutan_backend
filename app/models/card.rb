f593984c3640dd499efe1ccabec464c068f6b960
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-11-18 20:28:17 +0900

    feat: add assoiation and validation on card model #30

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,9 @@
 class Card < ApplicationRecord
+  # Associations
   belongs_to :flashcard
+
+  # Validation
+  validates :front, :back, length: { maximum: 255 }, presence: true
+  validates :front_sentence, :back_sentence, :explanation, length: { maximum: 1000 }, presence: true
+  validates :front, :back, length: { maximum: 255 }, presence: true
 end
