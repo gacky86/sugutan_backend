a24f8263bdaefbf7ed352f1ca58f1d549757d5f5
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-11-18 17:19:26 +0900

    feat: create Flashcard model #27

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_04_020742) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_18_081711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_04_020742) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "flashcards", force: :cascade do |t|
+    t.string "description"
+    t.string "title"
+    t.string "icon_color"
+    t.string "language"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flashcards_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -69,4 +80,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_04_020742) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "flashcards", "users"
 end
---
+++ spec/factories/flashcards.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :flashcard do
+    description { "MyString" }
+    title { "MyString" }
+    icon_color { "MyString" }
+    language { "MyString" }
+    user { nil }
+  end
+end
