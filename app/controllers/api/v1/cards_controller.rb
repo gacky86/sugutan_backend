4e39076298b71e274debf8cc445fdd9e94646133
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-11-19 18:05:31 +0900

    feat:

---
+++ app/controllers/api/v1/cards_controller.rb
@@ -0,0 +1,60 @@
+class Api::V1::CardsController < ApplicationController
+  # skip_before_action :authenticate_user!
+  before_action :set_flashcard
+  before_action :set_card, only: %i[show update destroy]
+
+  def index
+    @cards = @flashcard.cards
+    render json: @cards
+  end
+
+  def show
+    render json: @card
+  end
+
+  def create
+    @card = @flashcard.cards.build(card_params)
+    if @card.save
+      learning_factor = LearningFactor.new(card: @card)
+      if learning_factor.save
+        render json: { card: @card, learning_factor: learning_factor }, status: :ok
+      else
+        @card.destroy
+        render json: { errors: learning_factor.errors }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: @card.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @card.update(card_params)
+      render json: @card
+    else
+      render json: { errors: @card.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @card.destroy
+    render json: { message: 'This card was successfuly deleted!' }, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'card was not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.errors }, status: :unprocessable_entity
+  end
+
+  private
+
+  def set_flashcard
+    @flashcard = Flashcard.find(params[:flashcard_id])
+  end
+
+  def set_card
+    @card = @flashcard.cards.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:front, :back, :front_sentence, :back_sentence, :explanation)
+  end
+end
